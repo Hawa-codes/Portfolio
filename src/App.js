8cc2f2fb0ab340975a0f0f2df2ad322ebad78010
Hawa-codes <adediranhawa@gmail.com>
2026-02-24 10:27:21 +0100

    POrtfolio Complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Contact from "./components/Contact/contact";
+import Footer from "./components/Footer/footer";
+import Intro from "./components/Intro/intro";
+import Navbar from "./components/NavBar/navbar";
+import SKill from "./components/Skill/skill";
+import Works from "./components/Works/works";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Intro/>
+      <SKill/>
+      <Works/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Contact/contact.js
@@ -0,0 +1,67 @@
+import React, { useRef } from 'react'
+import './contact.css'
+import Walmart from '../../assets/walmart.png'
+import Adobe from '../../assets/adobe.png'
+import Microsoft from '../../assets/microsoft.png'
+import Facebook from '../../assets/facebook.png'
+import FacebookIcon from '../../assets/facebook-icon.png'
+import InstagramIcon from '../../assets/instagram.png'
+import TwitterIcon from '../../assets/twitter.png'
+import YouTubeIcon from '../../assets/youtube.png'
+import emailjs from '@emailjs/browser';
+
+const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_gl53fec', 'template_xjn2p1q', form.current, {
+        publicKey: '8m3U2Cn3b05Bs-r3ceaft',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
+  return (
+    <section id='contactPage'>
+        <div id='clients'>
+            <h1 className='contactPageTitle'>My Clients</h1>
+            <p className='clientDesc'>
+                I have had the opportunity to work with a diverse group of companies.
+                Some of the notable companies I have worked with includes
+            </p>
+            <div className='clientImgs'>
+                <img src={Walmart} alt='Walmart' className='clientImg'/>
+                <img src={Adobe} alt='Adobe' className='clientImg'/>
+                <img src={Microsoft} alt='Microsoft' className='clientImg'/>
+                <img src={Facebook} alt='Facebook' className='clientImg'/>
+            </div>
+        </div>
+        <div id='contact'>
+            <h1 className='contactPageTitle'>Contact Me</h1>
+            <span className='contactDesc'>Please fill the form below to discuss any work opportunities.</span>
+            <form className='contactForm' ref={form}onSubmit={{sendEmail}}>
+                <input type='text' className='name' placeholder='Your Name' name="your_name"/>
+                <input type='email' className='email' placeholder='Your Email'name="your_email" />
+                <textarea name='message' className='msg' rows='5' placeholder='Your Message'/>
+                <button type='submit' value='send' className='submitBtn'>Submit</button>
+                <div className='links'>
+                    <img src={FacebookIcon} alt='Facebook' className='link'/>
+                    <img src={TwitterIcon} alt='Twitter' className='link'/>
+                    <img src={YouTubeIcon} alt='YouTube' className='link'/>
+                    <img src={InstagramIcon} alt='Instagram' className='link'/>
+                </div>
+            </form>
+        </div>
+    </section>
+  )
+}
+
+export default Contact
